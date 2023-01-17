d51b0de550310f69ef02a877a1e11d722b6a98e4
Jiwon-kk <chajiwon3168@gmail.com>
2023-01-17 15:31:10 +0900

    암호화 회원가입/로그인

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-const { register } = require('./query');
-//const cypto = require('cypto');
+const { register, login } = require('./query');
+const crypto = require('crypto');
 
 /** 해당 id의 회원정보들 */
 exports.info = (ctx,next) => {
@@ -8,12 +8,12 @@ exports.info = (ctx,next) => {
     ctx.body = `${id} 회원에 대한 정보`;
 }
 
+/**회원가입 */
 exports.register = async (ctx,next) => {
-    //회원가입 처리 모듈
     let { email, password, name} = ctx.request.body;
-    //let result = crypto.pbkdf2Syn
+    let result = await crypto.pbkdf2Sync(password, process.env.APP_KEY, 50, 100, 'sha512')
 
-    let { affectedRows } = await register(email, password, name);
+    let { affectedRows } = await register(email, result.toString('base64'), name);
 
     if (affectedRows > 0) {
         let token = await generateToken ({name});
@@ -23,17 +23,19 @@ exports.register = async (ctx,next) => {
     }
 }
 
+/**로그인 */
 exports.login = async (ctx, next) => {
-    //로그인 모듈
-    let {id, pwd} = ctx.request.body;
+   let {email, password} = ctx.request.body;
+   let result = crypto.pbkdf2Sync(password, process.env.APP_KEY, 50, 100, 'sha512')
+    
+    let item = await login(email, result.toString('base64'));
 
-    let result = "";
-    if (id === 'admin' && pwd ==='1234') {
-        result = await generateToken({name: 'abc'});
+    if (item == null) {
+        ctx.body = {result: "fail"};
     } else {
-        result ="아이디 혹은 패스워드가 올바르지 않습니다.";
+        let token = await generateToken({name: item.name});
+        ctx.body = token;
     }
-    ctx.body = result;
 }
 
 let generateToken = (payload) => {
